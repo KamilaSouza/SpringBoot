3da494887e017954406ad2976d4c87d7b2adfa74
KamilaSouza <kamisouzas@gmail.com>
2021-02-23 18:43:46 -0300

    Adicionado Repository

---
+++ src/main/java/br/com/alura/forum/Repository/TopicoRepository.java
@@ -0,0 +1,8 @@
+package br.com.alura.forum.Repository;
+
+import br.com.alura.forum.modelo.Topico;
+import com.sun.xml.bind.v2.model.core.ID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+}
