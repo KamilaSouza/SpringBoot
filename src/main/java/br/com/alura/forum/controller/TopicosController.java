923ed73e8ae7fd875f42db426ee3f86c07ff464e
KamilaSouza <kamisouzas@gmail.com>
2021-03-01 12:02:48 -0300

    Adicionada maneira para se atualizar o tópico

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,6 +3,7 @@ package br.com.alura.forum.controller;
 import br.com.alura.forum.Repository.CursoRepository;
 import br.com.alura.forum.Repository.TopicoRepository;
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
@@ -52,5 +54,13 @@ public class TopicosController {
 
     }
 
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<TopicoDto> atualizar (@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
+        Topico topico = form.atualizar(id,topicoRepository);
+
+        return ResponseEntity.ok(new TopicoDto(topico));
+
+    }
 }
 
